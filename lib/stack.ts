ea47e2e84e04a9619f9cf75dd42479a095900626
Henrique Lima <henlima@amazon.com>
2021-04-19 11:54:42 +0100

    Add experiment first version

--- lib/stack.ts
+++ lib/stack.ts
@@ -1,24 +1,72 @@
-import * as s3 from '@aws-cdk/aws-s3'
+import * as ec2 from '@aws-cdk/aws-ec2'
+import * as iam from '@aws-cdk/aws-iam'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as autoscaling from '@aws-cdk/aws-autoscaling'
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2'
 import * as cdk from '@aws-cdk/core';
-import { LambdaExecuter } from './lambda-executer';
-import { MainApi } from './main-api';
 
 export class CdkDemoProjectStack extends cdk.Stack {
 
   constructor(scope: cdk.App, id: string, props: {}) {
     super(scope, id, props);
 
-    const lambdaExecuterQueue = new LambdaExecuter(this, "lambdaExecuter").queue;
+    const vpc = new ec2.Vpc(this, 'VPC');
 
-    const bucket = new s3.Bucket(this, "bucket", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,        
-      websiteIndexDocument: "index.html"
+    const dynamoDbEndpoint = vpc.addGatewayEndpoint('DynamoDbEndpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB,
     });
 
-    new MainApi(this, "api", {
-      bucket,
-      queues: {executeLambda: lambdaExecuterQueue}
-    })
+    dynamoDbEndpoint.addToPolicy(
+      new iam.PolicyStatement({
+        principals: [new iam.AnyPrincipal()],
+        actions: ['dynamodb:DescribeTable', 'dynamodb:ListTables'],
+        resources: ['*'],
+      }));
+
+    const dynamoTable = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING }
+    });
+
+    dynamoDbEndpoint.addToPolicy(
+      new iam.PolicyStatement({
+        principals: [new iam.AnyPrincipal()],
+        actions: ['dynamodb:DeleteItem', 'dynamodb:PutItem', 'dynamodb:UpdateItem', 'dynamodb:GetItem'],
+        resources: [dynamoTable.tableArn],
+      }));
+
+
+    const mySecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow ssh access to ec2 instances',
+      allowAllOutbound: true
+    });
+    mySecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world');
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1
+    });
   }
 }
 
